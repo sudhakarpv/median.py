1e661ea6b1f0c42d289f3d093e0a76fa8f3b4708
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-18 18:43:54 +0530

    Update median.py

--- median.py
+++ median.py
@@ -1,11 +1,11 @@
 # median.py
 def main():
     pass
+    w=input()
     n=input().split()
     l=list(n)
     k=sorted(l)
     s=len(l)//2
     print(l[s])
-
 if __name__ == '__main__':
     main()
